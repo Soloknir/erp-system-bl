474c9dc4bcb5dfc27602a8321e917194fe1644fd
mv.baranov <mv.baranov@tensor.ru>
2025-02-20 11:43:07 +0300

    Added sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: '586479',
+      database: 'erp-system',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
